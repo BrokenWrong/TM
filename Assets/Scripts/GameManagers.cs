a5ac6711eb2809b7d9edf1ba8f4366389e0592b9
wenwei <1013115235@qq.com>
2019-01-02 23:22:01 +0800

    TM

---
+++ Assets/Scripts/GameManagers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagers : MonoBehaviour {
+    public GameObject Begin;
+    public GameObject Pass;
+    private Passs passs;
+    // Use this for initialization
+
+    void Awake()
+    {
+        passs = Pass.transform.GetComponent<Passs>();
+    }
+
+    void Start () {
+        Begin.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BeginClick()
+    {
+        Begin.SetActive(false);
+        Pass.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Passs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passs : MonoBehaviour {
+    private Vector3[] vec3VA =
+    {
+        new Vector3(-630, -70, 0), new Vector3(-581, -370, 0), new Vector3(-279, -65, 0),
+        new Vector3(11, -88, 0), new Vector3(42, 259, 0), new Vector3(108, -335, 0),
+        new Vector3(436, 366, 0), new Vector3(716, 148, 0), new Vector3(695, -195, 0),
+        new Vector3(472, -437, 0),
+    };
+    public GameObject CheckBtn;
+    public Transform Check;
+
+    // Use this for initialization
+    void Start () {
+        LoadCheckBtn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadCheckBtn()
+    {
+        for (int i = 0; i < vec3VA.Length; i++)
+        {
+            Transform tf = Instantiate(CheckBtn, Check).transform;
+            tf.localPosition = vec3VA[i];
+        }
+    }
+}
