39b3944f502d2546b297504443916d35df1de2f7
wenwei <1013115235@qq.com>
2019-01-05 02:01:56 +0800

    TM

---
+++ Assets/Scripts/FastImgs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastImgs : MonoBehaviour {
+    public void OnEnter()
+    {
+        Debug.Log("enter");
+    }
+    public void OnExit()
+    {
+        Debug.Log("exit");
+    }
+}
